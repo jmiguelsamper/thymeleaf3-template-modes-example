ddc4aa31b0b7a4b0f24ec6277a9937ccb2628eb9
Jose Samper <jmiguelss@yahoo.es>
2015-10-30 07:12:03 +0000

    Updated to 3.0.0.BETA01

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Simple example of the new Thymeleaf 3 template modes.
 
 The example shows in a single application HTML, CSS and JAVASCRIPT template modes.
 
+The easiest way to build and run the application using Maven is to execute `mvn tomcat7:run` and
+point your browser to [http://localhost:8080/thymeleaf3-template-modes-example](http://localhost:8080/thymeleaf3-template-modes-example)
+~
--- src/main/java/com/thymeleafexamples/thymeleaf3/config/ThymeleafConfig.java
+++ src/main/java/com/thymeleafexamples/thymeleaf3/config/ThymeleafConfig.java
@@ -1,6 +1,8 @@
 package com.thymeleafexamples.thymeleaf3.config;
 
 import static com.thymeleafexamples.thymeleaf3.util.ArrayUtil.array;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +11,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 /**
  * Thymeleaf and Spring MVC configuration.
@@ -20,9 +22,16 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.thymeleafexamples")
-public class ThymeleafConfig extends WebMvcConfigurerAdapter {
+public class ThymeleafConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
     private static final String UTF8 = "UTF-8";
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     @Bean
     public ViewResolver htmlViewResolver() {
@@ -54,28 +63,31 @@ public class ThymeleafConfig extends WebMvcConfigurerAdapter {
         return resolver;
     }
 
-    private TemplateEngine templateEngine(TemplateResolver templateResolver) {
+    private TemplateEngine templateEngine(ITemplateResolver templateResolver) {
         SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.addTemplateResolver(templateResolver);
+        engine.setTemplateResolver(templateResolver);
         return engine;
     }
 
-    private TemplateResolver htmlTemplateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+    private ITemplateResolver htmlTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
         resolver.setPrefix("/WEB-INF/templates/");
         resolver.setTemplateMode(TemplateMode.HTML);
         return resolver;
     }
 
-    private TemplateResolver cssTemplateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+    private ITemplateResolver cssTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
         resolver.setPrefix("/WEB-INF/css/");
         resolver.setTemplateMode(TemplateMode.CSS);
         return resolver;
     }
 
-    private TemplateResolver javascriptTemplateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+    private ITemplateResolver javascriptTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
         resolver.setPrefix("/WEB-INF/js/");
         resolver.setTemplateMode(TemplateMode.JAVASCRIPT);
         return resolver;
